b3231e4af7d63c00b4d7b5c9d367fa4df47fcce2
Michal Szczepaniak <mr.szczepaniak@gmail.com>
2015-02-05 18:41:50 +0100

    Add Nunit test project

---
+++ Calculator/SimpleCalculator.cs
@@ -0,0 +1,16 @@
+﻿namespace Calculator
+{
+    public class SimpleCalculator
+    {
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public int Multiply(int a, int b)
+        {
+            // intentional bug
+            return a + b;
+        }
+    }
+}
