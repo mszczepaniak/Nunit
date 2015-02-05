487ff783d8c52a117df78d7234fba7d1df69db78
Michal Szczepaniak <mr.szczepaniak@gmail.com>
2015-02-05 19:42:45 +0100

    Write first test

---
+++ Calculator.Tests/Class1.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+
+namespace Calculator.Tests
+{
+    [TestFixture]
+    public class CalculatorTests
+    {
+        [Test]
+        public void ShouldAddTwoNumbers()
+        {
+            var sut = new SimpleCalculator();
+
+            var result = sut.Add(1,2);
+
+            Assert.That(result, Is.EqualTo(3));
+        }
+
+       
+    }
+}
